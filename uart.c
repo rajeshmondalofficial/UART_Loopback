0f579d68c22dacff96a910d197e86fdbcf2c1f0a
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-07 22:38:52 +0530

    Rajesh: Sleep

--- uart.c
+++ uart.c
@@ -51,7 +51,7 @@ int main()
         char read_buffer[256];
         int bytes_read = read(uart_fd, read_buffer, 256);
 
-        usleep(10000);
+        // usleep(10000);
 
         // When bytes read more than 0 make the reading flag true
         if (bytes_read > 0)
