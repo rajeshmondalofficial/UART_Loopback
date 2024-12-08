2fabad51dbb47a7a97c538baa6a9927e72cd7ed6
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-08 11:22:00 +0530

    Rajesh: Fixing

--- recv_uart.c
+++ recv_uart.c
@@ -50,7 +50,9 @@ int main()
             {
                 read_buffer[bytes_read] = '\0';
                 strcat(data, read_buffer);
-                // printf("Block Data: %s\n", read_buffer);
+                printf("Block Data: %s\n", read_buffer);
+                printf("Received Data: %d", bytes_read);
+                printf("Read Buffer Length: %d\n", strlen(read_buffer));
                 if (bytes_read > strlen(read_buffer))
                 {
                     dataReady = 1;
@@ -59,7 +61,7 @@ int main()
 
             if (dataReady > 0)
             {
-                printf("Received: %s\n", data);
+                // printf("Received: %s\n", data);
                 data[0] = '\0';
                 dataReady = -1;
             }
