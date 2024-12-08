12224c28f04ea79180dc9ba85cbcdba4eabfa273
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-08 12:10:02 +0530

    Rajesh: Fixing

--- recv_uart.c
+++ recv_uart.c
@@ -10,10 +10,10 @@
 #define BAUD_RATE B9600
 
 int main(int argc, char *argv[]) {
-    // if (argc < 2) {
-    //     fprintf(stderr, "Usage: %s <data_to_send>\n", argv[0]);
-    //     return 1;
-    // }
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <data_to_send>\n", argv[0]);
+        return 1;
+    }
 
     int uart_fd = open(UART_DEVICE, O_RDWR | O_NOCTTY);
     if (uart_fd < 0) {
@@ -30,40 +30,28 @@ int main(int argc, char *argv[]) {
     tcflush(uart_fd, TCIFLUSH);
     tcsetattr(uart_fd, TCSANOW, &options);
 
-    // char send_data[256];
-    // snprintf(send_data, sizeof(send_data), "%s\r\n", argv[1]);
+    char send_data[256];
+    snprintf(send_data, sizeof(send_data), "%s\r\n", argv[1]);
 
-    // int bytes_written = write(uart_fd, send_data, strlen(send_data));
-    // if (bytes_written < 0) {
-    //     perror("UART write failed");
-    //     close(uart_fd);
-    //     return 1;
-    // }
-    // printf("Sent: %s\n", send_data);
+    int bytes_written = write(uart_fd, send_data, strlen(send_data));
+    if (bytes_written < 0) {
+        perror("UART write failed");
+        close(uart_fd);
+        return 1;
+    }
+    printf("Sent: %s\n", send_data);
 
     char received_data[1024];
-    char buffer[1024];
-    int buffer_index = 0;
-
+    char data[1024];
     while (1) {
-        int bytes_read = read(uart_fd, buffer + buffer_index, sizeof(buffer) - buffer_index - 1);
+        int bytes_read = read(uart_fd, received_data, sizeof(received_data) - 1);
         if (bytes_read < 0) {
             perror("UART read failed");
             break;
         } else if (bytes_read > 0) {
-            buffer_index += bytes_read;
-            buffer[buffer_index] = '\0';
-
-            // Check if we have a complete message
-            char *newline_pos = strchr(buffer, '\n');
-            if (newline_pos != NULL) {
-                *newline_pos = '\0'; // Null-terminate the message
-                printf("Received: %s\n", buffer);
-
-                // Move remaining data to the beginning of the buffer
-                buffer_index = strlen(newline_pos + 1);
-                memmove(buffer, newline_pos + 1, buffer_index);
-            }
+            received_data[bytes_read] = '\0';
+            strcat(data, received_data);
+            printf("Received: %s\n", received_data);
         }
     }
 
