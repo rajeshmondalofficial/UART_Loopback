aca90e32fafc49356ece4fece2572e8c8b2f9c75
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-08 10:04:36 +0530

    Rajesh: Minor Fixing

--- uart.c
+++ uart.c
@@ -21,7 +21,7 @@ int main()
     struct termios options;
     tcgetattr(uart_fd, &options);
 
-    options.c_cflag = B9600 | CS16 | CLOCAL | CREAD; // Baud rate: 9600, 8 data bits, no parity, 1 stop bit
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD; // Baud rate: 9600, 8 data bits, no parity, 1 stop bit
     options.c_iflag = IGNPAR;                       // Ignore framing errors
     options.c_oflag = 0;
     options.c_lflag = 0; // Non-canonical mode
