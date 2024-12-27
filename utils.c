538802d76cd47e2cde2bd2253525aad8602ff34f
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-28 00:28:36 +0530

    thg

--- utils.c
+++ utils.c
@@ -1,4 +1,9 @@
 char* receive_message = "+RCV=50,5,Hello,-99,40";
 
-strtok_r(receive_message, "=", &receive_message);
-printf("%s", receive_message);
+char *saveptr;
+char *token;
+
+// Tokenize the string
+token = strtok_r(receive_message, "=", &saveptr);
+
+printf("%s", token);
