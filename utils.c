748e9ea42cce3610f771645d7253ca2aab79c785
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-12-28 00:33:24 +0530

    sjbs

--- utils.c
+++ utils.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-char* receive_message = "+RCV=50,5,Hello,-99,40";
+int main() {
+    char receive_message[] = "key=value=another"; // Example string
+    char *saveptr; // Pointer for strtok_r
+    char *token;
 
-char *saveptr;
-char *token;
+    // Tokenize the string
+    token = strtok_r(receive_message, "=", &saveptr);
+    while (token != NULL) {
+        printf("Token: %s\n", token);
+        token = strtok_r(NULL, "=", &saveptr); // Pass NULL to continue tokenizing
+    }
 
-// Tokenize the string
-token = strtok_r(receive_message, "=", &saveptr);
-
-printf("%s", token);
+    return 0;
+}
